7452999f723ae23b22529b91e7b8649c65e3abf5
Julian Gruber <julian@juliangruber.com>
2013-07-08 00:59:50 +0200

    initial terminal display

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var levelFS = require('level-fs');
 var levelUP = require('levelup');
 var levelJS = require('level-js');
-var term = require('./vendor/term');
 var bash = require('bashful');
+var Term = require('./vendor/term');
+var ready = require('domready');
 
 var db = levelUP('db', { db: levelJS });
 var fs = levelFS(db);
@@ -18,8 +19,27 @@ var sh = bash({
 });
 
 var shell = sh.createStream();
-shell.on('data', console.log.bind(console));
 window.shell = shell;
+
+/**
+ * Terminal.
+ */
+
+var term = new Term(80, 25);
+term.on('pm', console.log.bind(console))
+shell.pipe(term);
+term.on('key', function (k) {
+  var code = k.charCodeAt(0);
+  if (code == '13') k = '\n';
+  term.write(k);
+  shell.write(k);
+});
+
+ready(function () {
+  term.open();
+  document.addEventListener('keydown', term.keyDown.bind(term));
+  document.addEventListener('keypress', term.keyPress.bind(term));
+});
 
 function spawn (_cmd, args, opts) {
   if (!cmd[_cmd]) return;
